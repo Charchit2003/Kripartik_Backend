729cc489591d8222aaf7418e536d2a1f95b5a4fb
Charchit <charchitpaharia@gmail.com>
2024-09-13 15:35:48 +0530

    1 -  Clean the code

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const server = express();
 const mongoose = require('mongoose');
-const { createProduct } = require('./controller/Product');
 const productsRouter = require('./routes/Products');
 const categoriesRouter = require('./routes/Categories');
 const brandsRouter = require('./routes/Brands');
