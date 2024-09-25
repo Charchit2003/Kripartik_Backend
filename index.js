cd57cfb695a71efa6daacdcde51fc2d496851300
Charchit <charchitpaharia@gmail.com>
2024-09-25 12:55:14 +0530

    9- Test Commit

--- index.js
+++ index.js
@@ -161,7 +161,7 @@ server.post("/create-payment-intent", async (req, res) => {
   const { totalAmount, orderId } = req.body;
   // Create a PaymentIntent with the order amount and currency
   const paymentIntent = await stripe.paymentIntents.create({
-    amount: totalAmount, // for decimal compensation
+    amount: totalAmount*100, // for decimal compensation
     currency: "inr",
     automatic_payment_methods: {
       enabled: true,
